eda4e49c4db060a7bf23e868f5d9eabaeb2011d3
Mo Mumtaz <124875455+m-mumtaz@users.noreply.github.com>
2024-09-05 20:51:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+var menu = document.querySelector('#menu-icon');
+var navbar = document.querySelector('.navbar');
 
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('.active');
-}
-window.onscroll = () => {
-    menu.classList.remove('bx-x');
-    navbar.classList.remove('.active');
-}
+    menu.onclick = () => {
+        menu.classList.toggle('bx-x'); // Toggle the menu icon style
+        navbar.classList.toggle('active'); // Toggle the navbar visibility
+    }
+
+    window.onscroll = () => {
+        menu.classList.remove('bx-x'); // Remove the active menu icon style
+        navbar.classList.remove('active'); // Hide the navbar on scroll
+    }
